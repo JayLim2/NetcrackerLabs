c9a3ad90c7bc67cbff90c2965841b001ee521692
JayLim <ddx180874@gmail.com>
2017-12-09 22:19:20 +0400

    Вынес проверки названия книги и имени автора во Вью. Пофиксил проверку года (не принимало текущий год).

--- src/main/java/Models/AuthorsContainer.java
+++ src/main/java/Models/AuthorsContainer.java
@@ -14,47 +14,46 @@ public class AuthorsContainer {
     public AuthorsContainer() {
         authors = new LinkedList<Author>();
     }
-  
+
     @XmlElement(name = "authorList")
-    public List<Author> getAuthors(){
+    public List<Author> getAuthors() {
         return authors;
     }
 
-    public void setAuthors(List<Author> authors){
+    public void setAuthors(List<Author> authors) {
         this.authors = authors;
     }
 
-    public Author getAuthor(int id){
+    public Author getAuthor(int id) {
         return authors.get(id);
     }
 
-    public void addAuthor(Author author){
+    public void addAuthor(Author author) {
 
-        if(!isExists(author, authors) && isValid(author)) {
+        if (!isExists(author, authors) && isValid(author)) {
             author.setName(author.getName().trim());
             authors.add(author);
-            System.out.println("Author "+ "\""+author.getName()+"\"" + " added");
+            System.out.println("Author " + "\"" + author.getName() + "\"" + " added");
             sort();
         } else
             System.out.println("Authors name is not correct");
 
-        }
-
+    }
 
     public void addAuthor(int index, Author author) {
 
-            if (!isExists(author, authors) && isValid(author)) {
-                author.setName(author.getName().trim());
-                authors.add(index, author);
-                System.out.println("Author " + "\"" + author.getName() + "\"" + " added");
-            } else
-                System.out.println("Authors name is not correct");
+        if (!isExists(author, authors) && isValid(author)) {
+            author.setName(author.getName().trim());
+            authors.add(index, author);
+            System.out.println("Author " + "\"" + author.getName() + "\"" + " added");
+        } else
+            System.out.println("Authors name is not correct");
 
     }
 
     private boolean isExists(Author author, List<Author> authors) {
         for (Author currentAuthor : authors) {
-            if(author.equals(currentAuthor))
+            if (author.equals(currentAuthor))
                 return true;
         }
         return false;
@@ -65,7 +64,7 @@ public class AuthorsContainer {
                 author.getName().length() <= AUTHORNAME_LIMIT;
     }
 
-    private void sort(){
+    private void sort() {
         authors.sort(new AuthorComparator());
     }
 
