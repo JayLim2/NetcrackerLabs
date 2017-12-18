4bcdca4223a566172bb23761e18e7a30b55c3b93
RostislavKorostelev <korrost@mail.ru>
2017-12-19 00:15:55 +0400

    проверки в change, сортировка по id

--- src/main/java/Models/AuthorComparator.java
+++ src/main/java/Models/AuthorComparator.java
@@ -4,7 +4,9 @@ import java.util.Comparator;
 //Возвращает 1, если первая строка больше второй
 public class AuthorComparator implements Comparator<Author> {
     public int compare(Author author1, Author author2){
-            return author1.getName().compareTo(author2.getName());
+        Integer a = author1.getId();
+        Integer b = author2.getId();
+            return a.compareTo(b);
     }
 
 }
