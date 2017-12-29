9143c54599da1d9a440ddfd45b6f3c01cc5b04cb
Alexaderrand101git <Sonicrider2@yandex.ru>
2017-12-29 04:21:38 +0400

    это чтобы fxml копировался

--- Client/src/main/java/client/gui/Main.java
+++ Client/src/main/java/client/gui/Main.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("MainForm.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("..\\..\\MainForm.fxml"));
         primaryStage.setTitle("Лабораторная работа №2");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
